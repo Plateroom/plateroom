6a991eda4275f95af800bb2580c366e08ed5a1a5
Plateroom <stefania_solari@hotmail.it>
2015-06-23 16:55:13 +0200

    User ID, signIn, signUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :Users  
   root "pages#home"
   get "about" => "pages#about" # creates about_path
   # The priority is based upon order of creation: first created -> highest priority.
